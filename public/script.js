69ad6acffc5cfd9f4a2c491e07b1c1760e657816
Bote Zhou <botez4204@gmail.com>
2024-03-26 21:22:00 -0400

    adjust css for click events

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
-if (!isStartingPage) {
-    if (player1turn === "true") {
-        isPlayer1Turn = true;
-    } else {
-        isPlayer1Turn = false;
-    }
-
-    console.log(`Gamemode: ${gamemode}\nGameboard: ${gameboard}\nPlayer 1 go First? ${isPlayer1Turn}`)
+if (player1turn === "true") {
+    isPlayer1Turn = true;
+} else {
+    isPlayer1Turn = false;
 }
+
+console.log(`Gamemode: ${gamemode}\nGameboard: ${gameboard}\nPlayer 1 go First? ${isPlayer1Turn}`);
